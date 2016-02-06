a8b89a838e4ee027ad9b59fe3fdbaf8b1b1fbc65
Alexander Reitzel <funtimecoding@gmail.com>
2016-02-06 18:22:37 +0100

    sync with base skeleton

--- init-project.sh
+++ init-project.sh
@@ -23,5 +23,5 @@ fi
 DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
 rm init-project.sh sync-project.sh
 # shellcheck disable=SC2016
-${FIND} . -type f -regextype posix-extended ! -regex '^.*/(\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/ScalaSkeleton/${2}/g" -e "s/scala-skeleton/${3}/g" ${4}' '_' "${SED}" "${CAMEL}" "${DASH}" '{}' \;
+${FIND} . -type f -regextype posix-extended ! -regex '^.*/(\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/ScalaSkeleton/${2}/g" -e "s/scala-skeleton/${3}/g" "${4}"' '_' "${SED}" "${CAMEL}" "${DASH}" '{}' \;
 echo "Done. Files were edited and moved. Review those changes."
